332fc7fda6b0fb7512d908f72f367c519ef04a51
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-01-13 02:59:09 +0100

    merlinx: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: Redmi Note 9 Script version: 3.0.0

---
+++ omni_merlinx.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from merlinx device
+$(call inherit-product, device/xiaomi/merlinx/device.mk)
+
+PRODUCT_DEVICE := merlinx
+PRODUCT_NAME := omni_merlinx
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 9
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="merlin-user 11 RP1A.200720.011 V12.5.4.0.RJOMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/merlin/merlin:11/RP1A.200720.011/V12.5.4.0.RJOMIXM:user/release-keys
